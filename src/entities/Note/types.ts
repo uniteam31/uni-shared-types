08dc588a851cfd34e2f40e9101a59f2cb9300f6d
Daniil <temnay2@icloud.com>
2025-02-16 19:23:30 +0300

    UNI-97: replace absolute imports to relative

--- src/entities/Note/types.ts
+++ src/entities/Note/types.ts
@@ -1,14 +1,18 @@
-import { TMeta } from 'shared/api';
+// TODO
+/* Не смог разобраться с тем, чтобы заменить абсолютный импорт на относительный или
+ * сделать нормальное поведение при сборке
+ * */
+import type { TMeta } from '../../shared/api';
 
 /** Поля у формы заметки */
 export type TNoteFormFields = {
-    title?: string;
-    text?: string;
+	title?: string;
+	text?: string;
 };
 
 /** Данные заметки */
 export type TNodeData = TNoteFormFields & {
-    author: string;
+	author: string;
 };
 
 /** Целый экземпляр */
