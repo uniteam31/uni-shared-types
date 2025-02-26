629206a018fc6fb478b0f53e01258c023e670355
Daniil <temnay2@icloud.com>
2025-02-26 16:07:14 +0300

    feature/UNISHARE-20: update User types

--- src/entities/User/types.ts
+++ src/entities/User/types.ts
@@ -1,8 +1,16 @@
 import type { TMeta } from '../../shared/api';
 
-export interface IUser {
+export type TPublicUser = {
 	_id: TMeta['_id'];
 	firstName: string;
+	lastName?: string;
 	username: string;
 	avatar?: string;
-}
+};
+
+export type TPrivateUser = {
+	email: string;
+	educationalEmail?: string;
+};
+
+export interface IUser extends TPublicUser, TPrivateUser {}
